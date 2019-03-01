1cfe51d7c063268f45d4d16a5f735f558aeaf54d
Emmy Armintrout <emmy@resource.io>
2019-03-01 09:44:45 -0800

    fix routing i

--- server/index.js
+++ server/index.js
@@ -24,12 +24,15 @@ app.use(cors());
 app.use(morgan('dev'));
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('RUNNING IN PROD');
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+  app.get('/cool', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 const getMe = async (req) => {
   const token = req.headers['x-token'];
